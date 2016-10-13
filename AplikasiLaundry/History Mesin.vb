14cec08199c0033b38adb79145c73ce52a770307
bagus811 <bagusindrawan811@gmail.com>
2016-10-13 23:02:32 +0700

    History Mesin
    History Mesin

---
+++ AplikasiLaundry/History Mesin.vb
@@ -0,0 +1,3 @@
+﻿Public Class History_Mesin
+
+End Class
--- AplikasiLaundry/Master/Master Mesin.Designer.vb
+++ AplikasiLaundry/Master/Master Mesin.Designer.vb
@@ -22,8 +22,261 @@ Partial Class Master_Mesin
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Master_Mesin))
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.tb_idmesin = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.tb_jenis = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tb_namamesin = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_update = New System.Windows.Forms.Button()
+        Me.btn_insert = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(486, 102)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(494, 494)
+        Me.DataGridView1.TabIndex = 61
+        '
+        'tb_idmesin
+        '
+        Me.tb_idmesin.Enabled = False
+        Me.tb_idmesin.Location = New System.Drawing.Point(192, 18)
+        Me.tb_idmesin.Name = "tb_idmesin"
+        Me.tb_idmesin.Size = New System.Drawing.Size(202, 24)
+        Me.tb_idmesin.TabIndex = 60
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.tb_idmesin)
+        Me.Panel1.Controls.Add(Me.btn_delete)
+        Me.Panel1.Controls.Add(Me.btn_update)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.MaskedTextBox1)
+        Me.Panel1.Controls.Add(Me.btn_insert)
+        Me.Panel1.Controls.Add(Me.tb_jenis)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.tb_namamesin)
+        Me.Panel1.Font = New System.Drawing.Font("Footlight MT Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.ForeColor = System.Drawing.Color.Yellow
+        Me.Panel1.Location = New System.Drawing.Point(19, 102)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(432, 483)
+        Me.Panel1.TabIndex = 60
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(32, 194)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 18)
+        Me.Label6.TabIndex = 56
+        Me.Label6.Text = "Catatan"
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(192, 143)
+        Me.MaskedTextBox1.Mask = "(999) 000000000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(202, 24)
+        Me.MaskedTextBox1.TabIndex = 52
+        '
+        'tb_jenis
+        '
+        Me.tb_jenis.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tb_jenis.Location = New System.Drawing.Point(192, 103)
+        Me.tb_jenis.Multiline = True
+        Me.tb_jenis.Name = "tb_jenis"
+        Me.tb_jenis.Size = New System.Drawing.Size(202, 24)
+        Me.tb_jenis.TabIndex = 47
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Footlight MT Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Yellow
+        Me.Label5.Location = New System.Drawing.Point(32, 143)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(97, 18)
+        Me.Label5.TabIndex = 51
+        Me.Label5.Text = "Nomer Mesin"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Footlight MT Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Yellow
+        Me.Label4.Location = New System.Drawing.Point(32, 106)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 18)
+        Me.Label4.TabIndex = 50
+        Me.Label4.Text = "Jenis"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Footlight MT Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Yellow
+        Me.Label3.Location = New System.Drawing.Point(32, 61)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 18)
+        Me.Label3.TabIndex = 49
+        Me.Label3.Text = "Nama Mesin"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Footlight MT Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Yellow
+        Me.Label2.Location = New System.Drawing.Point(32, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 18)
+        Me.Label2.TabIndex = 48
+        Me.Label2.Text = "ID Mesin"
+        '
+        'tb_namamesin
+        '
+        Me.tb_namamesin.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tb_namamesin.Location = New System.Drawing.Point(192, 58)
+        Me.tb_namamesin.Name = "tb_namamesin"
+        Me.tb_namamesin.Size = New System.Drawing.Size(202, 24)
+        Me.tb_namamesin.TabIndex = 46
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Maiandra GD", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(5, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(448, 64)
+        Me.Label1.TabIndex = 59
+        Me.Label1.Text = "Master Mesin"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btn_delete
+        '
+        Me.btn_delete.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btn_delete.ForeColor = System.Drawing.Color.Black
+        Me.btn_delete.Image = CType(resources.GetObject("btn_delete.Image"), System.Drawing.Image)
+        Me.btn_delete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btn_delete.Location = New System.Drawing.Point(306, 391)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Padding = New System.Windows.Forms.Padding(11, 0, 10, 0)
+        Me.btn_delete.Size = New System.Drawing.Size(96, 32)
+        Me.btn_delete.TabIndex = 59
+        Me.btn_delete.Text = "Delete"
+        Me.btn_delete.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btn_delete.UseVisualStyleBackColor = True
+        '
+        'btn_update
+        '
+        Me.btn_update.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btn_update.ForeColor = System.Drawing.Color.Black
+        Me.btn_update.Image = CType(resources.GetObject("btn_update.Image"), System.Drawing.Image)
+        Me.btn_update.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btn_update.Location = New System.Drawing.Point(166, 391)
+        Me.btn_update.Name = "btn_update"
+        Me.btn_update.Padding = New System.Windows.Forms.Padding(8, 0, 6, 0)
+        Me.btn_update.Size = New System.Drawing.Size(96, 32)
+        Me.btn_update.TabIndex = 58
+        Me.btn_update.Text = "Update"
+        Me.btn_update.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btn_update.UseVisualStyleBackColor = True
+        '
+        'btn_insert
+        '
+        Me.btn_insert.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btn_insert.ForeColor = System.Drawing.Color.Black
+        Me.btn_insert.Image = CType(resources.GetObject("btn_insert.Image"), System.Drawing.Image)
+        Me.btn_insert.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btn_insert.Location = New System.Drawing.Point(33, 391)
+        Me.btn_insert.Name = "btn_insert"
+        Me.btn_insert.Padding = New System.Windows.Forms.Padding(13, 0, 12, 0)
+        Me.btn_insert.Size = New System.Drawing.Size(96, 32)
+        Me.btn_insert.TabIndex = 55
+        Me.btn_insert.Text = "Insert"
+        Me.btn_insert.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btn_insert.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(192, 191)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(202, 24)
+        Me.TextBox1.TabIndex = 61
+        '
+        'Master_Mesin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.AplikasiLaundry.My.Resources.Resources.colorful_triangles_background
+        Me.ClientSize = New System.Drawing.Size(985, 603)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Master_Mesin"
         Me.Text = "Master_Mesin"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents tb_idmesin As System.Windows.Forms.TextBox
+    Friend WithEvents btn_update As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btn_delete As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents MaskedTextBox1 As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents btn_insert As System.Windows.Forms.Button
+    Friend WithEvents tb_jenis As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents tb_namamesin As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
 End Class
