e24fa8d423f074d1eb916ab96636535ab4f893d7
aditcsc96 <aditya.raharjo94@gmail.com>
2016-10-19 21:05:44 +0700

    Delete data
    19 Oktober 2016 - 09:05PM

--- AplikasiLaundry/Report/FormTransTanggal.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FormTransTanggal
-
-    Private Sub FormTransTanggal_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
-        FormUtama.ChildNumber -= 1
-    End Sub
-End Class
--- AplikasiLaundry/Report/LaporanMember.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class LaporanMember
-
-    Private Sub LaporanMember_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
-        FormUtama.ChildNumber -= 1
-    End Sub
-End Class
